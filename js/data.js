0a495f7e6c7ab0aeebd234fd7048eb6b7872ee91
Leanne Vu <leannevu1@yahoo.com>
2022-11-01 23:37:15 +0000

    Listen for 'submit' events on the journal entry form.

--- js/data.js
+++ js/data.js
@@ -8,3 +8,12 @@ var data = {
 };
 
 // local storage exercise
+var previousDataJSON = localStorage.getItem('entry-storage');
+if (previousDataJSON !== null) {
+  data = JSON.parse(previousDataJSON);
+}
+
+window.addEventListener('beforeunload', function (event) {
+  var dataJSON = JSON.stringify(data);
+  localStorage.setItem('entry-storage', dataJSON);
+});
--- js/main.js
+++ js/main.js
@@ -1,8 +1,23 @@
 /* global data */
 var $photoUrlInput = document.querySelector('#photo-url');
 var $image = document.querySelector('img');
-
+var $form = document.querySelector('form');
 $photoUrlInput.addEventListener('input', function () {
   $image.setAttribute('src', event.target.value);
+}
+);
+
+$form.addEventListener('submit', function () {
+  event.preventDefault();
+  var entry = {
+    title: $form.elements.title.value,
+    url: $form.elements.url.value,
+    notes: $form.elements.notes.value,
+    EntryID: data.nextEntryId
+  };
+  data.nextEntryId++;
+  data.entries.push(entry);
+  $image.setAttribute('src', 'images/placeholder-image-square.jpg');
+  $form.reset();
 }
 );
